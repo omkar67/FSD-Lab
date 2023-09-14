a6ac40e7ee8281efede00e900b9fb98f8c3076e5
Omkar <omkar18rajmane@gmail.com>
2023-09-14 09:28:42 +0530

    cloned git repo and added a file

---
+++ test2.js
@@ -0,0 +1,59 @@
+let arr=[1,2,34,5,653,43]
+let var123={1:'a',2:'b'}
+let a=10
+let f="100"
+let b=2123
+let c=true
+let d=null
+let e=63712
+console.log(typeof(arr))
+console.log(typeof(var123))
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+console.log(e+b)
+console.log(e-b)
+console.log(e*b)
+console.log(e/b)
+console.log(e%b)
+console.log(a--)
+console.log(--a)
+console.log(++a)
+console.log(a++)
+console.log(a+ f)
+
+console.log(Math.pow(a,2))
+console.log(Math.floor(5/2))
+console.log(Math.ceil(5/2))
+
+var t=(20<18) ? "For true":"For False";
+console.log(t)
+
+var h=182
+var s=1829
+
+console.log((s>b)&&(s<b))
+console.log((s>b)||(s<b))
+
+var date= new Date();
+var currYear=date.getFullYear();
+console.log(typeof(currYear))
+var yr='1998';
+var age=currYear-yr
+console.log("Age is "+age)
+arr.push(3682)
+console.log(arr)
+arr.pop()
+console.log(arr)
+console.log(arr.toString())
+console.log(arr.shift())
+console.log(arr.join('+'))
+arr.unshift(100)
+console.log(arr)
+arr.splice(1,2,'lemom','kiwi')
+console.log(arr)
+console.log(arr.slice(1,4))
+
+
